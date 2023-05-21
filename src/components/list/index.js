65cb3aa015b09cbace1fed0496b8a57f776fd522
Kromus22 <kromus2@gmail.com>
2023-05-22 01:05:30 +0500

    изменяет List чтобы мог применять разные Item

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -2,14 +2,23 @@ import React from "react";
 import PropTypes from 'prop-types';
 import Item from "../item";
 import './style.css';
+import CartItem from "../cart-item";
 
-function List({ list, onAddToCart }) {
+function List(props) {
 
   return (
     <div className='List'>{
-      list.map(item =>
+      props.list && props.list.map(item =>
         <div key={item.code} className='List-item'>
-          <Item item={item} onAddToCart={onAddToCart} />
+          <Item item={item}
+            onAddToCart={props.onAddToCart} />
+        </div>
+      )}
+
+      {props.cartList && props.cartList.map(item =>
+        <div key={item.code} className='Modal-list'>
+          <CartItem item={item}
+            onDelete={props.onDelete} />
         </div>
       )}
     </div>
@@ -19,7 +28,7 @@ function List({ list, onAddToCart }) {
 List.propTypes = {
   list: PropTypes.arrayOf(PropTypes.shape({
     code: PropTypes.number
-  })).isRequired,
+  })),
   onAddToCart: PropTypes.func,
 };
 
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './style.css';
 import CartItem from "../cart-item";
+import List from "../list";
 
 // какое-то время ковырял Item, чтобы юзать его и тут, и в List
 // в итоге почитал сообщения в чате, кажется, что это не нужно
@@ -15,20 +16,9 @@ function Modal({ cartList, setModal, total, onDeleteItem }) {
         <button className={'Modal-close'} onClick={() => setModal(false)}>Закрыть</button>
       </div>
 
-      <div className={'Modal-list'}>
-        {cartList.length > 0 ?
-          cartList.map(item =>
-            <div key={item.code} className='List-item'>
-              <CartItem
-                item={item}
-                onDelete={onDeleteItem}
-              />
-            </div>
-          ) :
-          <div className="Modal-empty">Ваша корзина пуста</div>
-        }
-
-      </div>
+      <List
+        cartList={cartList}
+        onDelete={onDeleteItem} />
       <div className={'Modal-footer'}>
         <h3 className={'modal-footer_title'}>Итого</h3>
         <h3 className={'Modal-count'}>{total.toLocaleString('ru-RU')} ₽</h3>
